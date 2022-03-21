67b4bd1ad050e60b00d404f87964c72c90f28200
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-03-21 18:38:38 +0900

    알고리즘 커밋

---
+++ 평택에서 뭐하지/js/start.js
@@ -0,0 +1,50 @@
+var point =0;
+function resultalone(){
+    point=point+1;
+}
+function resulttwo(){
+    point=point+7;
+}
+function resultlittle(){
+    point=point+0;
+}
+function resultmuch(){
+    point=point+3;
+}
+function resullittletime(){
+    point=point+0;
+}
+function resultnormal(){
+    point=point+1;
+}
+function resultmany(){
+    point=point+2;
+}
+function resultShow(){
+    if(point == 1){
+    
+        window.open('result.html');
+    }else if(point==2){
+        window.open("result1.html");
+    }else if(point==3){
+        window.open("result2.html");
+    }else if(point==4){
+        window.open("result3.html")
+    }else if(point==5){
+        window.open("result4.html")
+    }else if(point==6){
+        window.open("result5.html")
+    }else if(point==7){
+        window.open("result6.html")
+    }else if(point==8){
+        window.open("result7.html")
+    }else if(point==9){
+        window.open("result8.html")
+    }else if(point==10){
+        window.open("result9.html")
+    }else if(point==11){
+        window.open("result10.html")
+    }else if(point==12){
+        window.open("result11.html")
+    }
+}
